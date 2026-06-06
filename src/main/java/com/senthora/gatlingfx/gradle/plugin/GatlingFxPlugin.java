4f7496058ebadf540f634401a2af66109cbb761c
meowpow-png <marin.softic@proton.me>
2026-06-07 01:31:57 +0200

    Create plugin entrypoint class

---
+++ src/main/java/com/senthora/gatlingfx/gradle/plugin/GatlingFxPlugin.java
@@ -0,0 +1,25 @@
+package com.senthora.gatlingfx.gradle.plugin;
+
+import com.senthora.gatlingfx.gradle.api.GatlingFxExtension;
+import com.senthora.gatlingfx.gradle.api.GatlingFxPluginConfigurer;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * Gradle plugin for GatlingFx.
+ */
+@SuppressWarnings("unused")
+public final class GatlingFxPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        var extension = project.getExtensions().create(
+                GatlingFxExtension.name(),
+                GatlingFxExtension.class
+        );
+       GatlingFxPluginConfigurer
+               .create(project, extension)
+               .configure();
+    }
+}
