e45c9b5198ee2ff6f76295429f578ab27f9c9a33
meowpow-png <marin.softic@proton.me>
2026-06-07 03:45:58 +0200

    Write source set provider integration tests

---
+++ src/integrationTest/java/com/senthora/gatlingfx/gradle/internal/DefaultSourceSetProviderTest.java
@@ -0,0 +1,54 @@
+package com.senthora.gatlingfx.gradle.internal;
+
+import com.senthora.gatlingfx.gradle.internal.support.SourceSets;
+
+import org.gradle.api.Project;
+import org.gradle.api.UnknownDomainObjectException;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.testfixtures.ProjectBuilder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DefaultSourceSetProviderTest {
+
+    private Project project;
+
+    @BeforeEach
+    void setupDefaultSourceSetProviderTest() {
+        project = ProjectBuilder.builder().build();
+    }
+
+    @Test
+    @DisplayName("Should return source sets when source set names exist")
+    void should_ReturnSourceSets_when_SourceSetNamesExist() {
+        var sourceSets = SourceSets.create(project);
+
+        var provider = new DefaultSourceSetProvider(project);
+        var actual = provider.get(Set.of(
+                SourceSet.MAIN_SOURCE_SET_NAME,
+                SourceSet.TEST_SOURCE_SET_NAME
+        ));
+        assertThat(actual).containsExactlyInAnyOrder(
+                sourceSets.main(),
+                sourceSets.test()
+        );
+    }
+
+    @Test
+    @DisplayName("Should throw UnknownDomainObjectException when source set does not exist")
+    void should_ThrowUnknownDomainObjectException_when_SourceSetDoesNotExist() {
+        SourceSets.create(project);
+
+        var provider = new DefaultSourceSetProvider(project);
+
+        assertThatThrownBy(() -> provider.get(Set.of("missing")))
+                .isInstanceOf(UnknownDomainObjectException.class);
+    }
+}
